a889f6f6610ce17e285178eb14c7a67f74d431fa
Jehan Fernando <jehanfernando2425@gmail.com>
2023-08-02 12:33:41 +0530

    Build a reponsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
     <div>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          NAV
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="red">
+            ASIDE
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="yellow">
+          MAIN
+        </GridItem>
+      </Grid>
     </div>
   );
 };
