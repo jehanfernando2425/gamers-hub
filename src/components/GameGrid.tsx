5e5f7e049ffc3ac716234b73530fbd11bea618f7
Jehan Fernando <jehanfernando2425@gmail.com>
2023-08-03 11:26:04 +0530

    Adding a sort selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={3} padding={10}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={3} padding={3}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
