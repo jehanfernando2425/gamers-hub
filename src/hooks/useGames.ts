bd700ba7bb5057ae80b963fb8c16e3ad5cc41806
Jehan Fernando <jehanfernando2425@gmail.com>
2023-08-18 01:30:11 +0530

    Fetch game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -8,9 +8,11 @@ const apiClient = new APIClient<Game>('/games');
 export interface Game {
     id: number;
     name: string;
+    slug:string;
     background_image:string;
     parent_platforms:{platform:Platforms}[];
     metacritic: number;
+    description_raw:string; 
   }
 
 const useGames = () => {
