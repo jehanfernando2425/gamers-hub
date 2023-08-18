91b05ccdf40792ae4df60adf4b94c16768ebca50
Jehan Fernando <jehanfernando2425@gmail.com>
2023-08-18 12:05:23 +0530

    Add a video preview

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,20 @@
+import useTrailers from "../hooks/useTrailers";
+import { Spinner } from "@chakra-ui/spinner";
+
+interface Props {
+  gameID: number;
+}
+
+const GameTrailer = ({ gameID }: Props) => {
+  const { data, error, isLoading } = useTrailers(gameID);
+
+  if (isLoading) return <Spinner />;
+  if (error) throw error;
+
+  const first = data?.results[0];
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls />
+  ) : null;
+};
+
+export default GameTrailer;
