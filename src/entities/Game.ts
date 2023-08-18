1b62cf88b48ce3ba004dcee4fd023ba7affd057c
Jehan Fernando <jehanfernando2425@gmail.com>
2023-08-18 11:32:14 +0530

    Build Game Attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,10 +1,14 @@
+import { Genre } from "./Genre";
 import { Platforms } from "./Platforms";
+import { Publisher } from "./Publisher";
 
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres:Genre[];
+  publishers:Publisher[];
   background_image: string;
   parent_platforms: { platform: Platforms; }[];
   metacritic: number;
